03b00f191f452542825ba53cc0699d9a54586f16
Joe Lawson <j.lawson.econ@gmail.com>
2023-02-22 09:38:46 +0000

    fix error in frontend build name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend_react_buildfoooobarrr')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend_react_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
